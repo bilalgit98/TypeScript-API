ea06bf8d30a0ee15a3c0fc8828e6d9dbe425cc9b
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-12-16 19:04:36 +0000

    CRUD Requests using RapidAPI Client

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,20 @@ const PORT = process.env.PORT || 5000;
 
 //ROUTES
 app.get("/api/jobs", (req: Request, res: Response) => {
-  res.json({ msg: "HELLO" });
+  res.json({ msg: "HELLO get" });
 });
 
+app.post("/api/jobs", (req: Request, res: Response) => {
+  res.json({ msg: "HELLO post" });
+});
+
+app.put("/api/jobs", (req: Request, res: Response) => {
+  res.json({ msg: "HELLO put" });
+});
+
+app.delete("/api/jobs/:id", (req: Request, res: Response) => {
+  res.json({ msg: "HELLO id delete" });
+});
 app.listen(PORT, () => {
   console.log(`SERVER STARTED ON PORT ${PORT}, http://localhost:${PORT}`);
 });
