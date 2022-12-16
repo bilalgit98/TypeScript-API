3cd6c424ab53cea59be8310f3d5549977f26a6ce
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-12-16 18:44:46 +0000

    Basics using Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello");
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+const app: Express = express();
+const PORT = process.env.PORT || 5000;
+
+//ROUTES
+app.get("/api/jobs", (req: Request, res: Response) => {
+  res.json({ msg: "HELLO" });
+});
+
+app.listen(PORT, () => {
+  console.log(`SERVER STARTED ON PORT ${PORT}, http://localhost:${PORT}`);
+});
