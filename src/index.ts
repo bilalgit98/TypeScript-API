776f3aeb3902cb060def7663712dc30fcc1f114c
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-12-18 22:18:55 +0000

    Try and Catch Handling Errors

--- src/index.ts
+++ src/index.ts
@@ -12,27 +12,47 @@ const xata = getXataClient();
 
 //ROUTES
 app.get("/api/jobs", async (req: Request, res: Response) => {
-  const job = await xata.db.jobs.getAll();
-  res.json(job);
+  try {
+    const job = await xata.db.jobs.getAll();
+    res.json(job);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "SOMETHING WENT WRONG PLEASE TRY AGAIN!" });
+  }
 });
 
 app.post("/api/jobs", async (req: Request, res: Response) => {
-  const job = req.body;
-  const createdJob = await xata.db.jobs.create(job);
-  res.json(createdJob);
+  try {
+    const job = req.body;
+    const createdJob = await xata.db.jobs.create(job);
+    res.json(createdJob);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "SOMETHING WENT WRONG PLEASE TRY AGAIN!" });
+  }
 });
 
 app.put("/api/jobs/:id", async (req: Request, res: Response) => {
-  const id = req.params.id;
-  const job = req.body;
-  const updatedJob = await xata.db.jobs.update(id, job);
-  res.json(updatedJob);
+  try {
+    const id = req.params.id;
+    const job = req.body;
+    const updatedJob = await xata.db.jobs.update(id, job);
+    res.json(updatedJob);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "SOMETHING WENT WRONG PLEASE TRY AGAIN!" });
+  }
 });
 
 app.delete("/api/jobs/:id", async (req: Request, res: Response) => {
-  const id = req.params.id;
-  const deletedRecord = await xata.db.jobs.delete(id);
-  res.json(deletedRecord);
+  try {
+    const id = req.params.id;
+    const deletedRecord = await xata.db.jobs.delete(id);
+    res.json(deletedRecord);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "SOMETHING WENT WRONG PLEASE TRY AGAIN!" });
+  }
 });
 app.listen(PORT, () => {
   console.log(`SERVER STARTED ON PORT ${PORT}, http://localhost:${PORT}`);
