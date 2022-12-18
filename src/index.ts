85c1d9d57fb4b6a43b5484d8c6c85e593d121d45
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-12-18 17:37:07 +0000

    Xata Setup and Xata in Express.js CRUD Endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,38 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { getXataClient, Jobs } from "./xata";
 
+dotenv.config();
 const app: Express = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+
+const xata = getXataClient();
+
 //ROUTES
-app.get("/api/jobs", (req: Request, res: Response) => {
-  res.json({ msg: "HELLO get" });
+app.get("/api/jobs", async (req: Request, res: Response) => {
+  const job = await xata.db.jobs.getAll();
+  res.json(job);
 });
 
-app.post("/api/jobs", (req: Request, res: Response) => {
-  res.json({ msg: "HELLO post" });
+app.post("/api/jobs", async (req: Request, res: Response) => {
+  const job = req.body;
+  const createdJob = await xata.db.jobs.create(job);
+  res.json(createdJob);
 });
 
-app.put("/api/jobs", (req: Request, res: Response) => {
-  res.json({ msg: "HELLO put" });
+app.put("/api/jobs/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const job = req.body;
+  const updatedJob = await xata.db.jobs.update(id, job);
+  res.json(updatedJob);
 });
 
-app.delete("/api/jobs/:id", (req: Request, res: Response) => {
-  res.json({ msg: "HELLO id delete" });
+app.delete("/api/jobs/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const deletedRecord = await xata.db.jobs.delete(id);
+  res.json(deletedRecord);
 });
 app.listen(PORT, () => {
   console.log(`SERVER STARTED ON PORT ${PORT}, http://localhost:${PORT}`);
